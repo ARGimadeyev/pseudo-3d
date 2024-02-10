79974be2c888262cd2fd4306dd056f0bb9053528
ARGimadeyev <ARGimadeyev@stud.kpfu.ru>
2024-02-10 21:11:38 +0300

    добавил стены и тригонометрическую функцию

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 using ld = long double;
 const ld INF = 1e9;
-const ld deg = 4;
+const ld deg = 3;
 const ld PI = acos(-1);
 
 void forStart() {
@@ -99,20 +99,33 @@ struct Ray {
     }
 };
 
-ld minDistance(const vector<pair<Point, Point>> &edge, Ray &r) {
+int indx;
+
+ld minDistance(const vector <pair<Point, Point>> &edge, Ray &r) {
     ld mn = INF;
+    int i = indx = 0;
     for (auto [a, b]: edge) {
         Point p = r.inters(a, b);
-        if (p.x != -INF)
-            mn = min(mn, (p - r.p).len());
+        if (p.x != -INF && (p - r.p).len() < mn)
+            mn = (p - r.p).len(), indx = i;
+        i++;
+    }
+    i = 0;
+    for (auto [a, b]: edge) {
+        Point p = r.inters(a, b);
+        if (p.x != -INF && abs((p - r.p).len() - mn) <= 1e-7 && indx != i) {
+            indx = -1;
+            break;
+        }
+        i++;
     }
     return mn;
 }
 
 // geometry*************************************************************************************************************
-const int W = 236, H = 55;
+const int W = 284, H = 65;
 char screen[H][W];
-vector<pair<Point, Point>> edges;
+vector <pair<Point, Point>> edges;
 #define sky 0
 #define vall 1
 #define floor 2
@@ -135,11 +148,14 @@ void output() {
                 case vall:
                     printf("\u2588");
                     break;
+                case 69:
+                    printf("|");
+                    break;
                 case floor:
                     if (i % 2 == j % 2 && timer % 2 || i % 2 != j % 2 && timer % 2 == 0) {
-                        printf("#");
+                        printf("o");
                     } else {
-                        printf("0");
+                        printf(".");
                     }
                     break;
                 case '#':
@@ -161,7 +177,7 @@ int32_t main() {
     ifstream fin("Input.txt");
     int n, m;
     fin >> n >> m;
-    vector<string> map(n + 1);
+    vector <string> map(n + 1);
     for (int i = 0; i < n; ++i) {
         fin >> map[i];
         map[i] += '*';
@@ -178,8 +194,20 @@ int32_t main() {
                 if (map[i][j] != map[i][j + 1]) {
                     Point a1((i + 1) * 10, (x + 1) * 10),
                             a2((i + 1) * 10, (j + 1) * 10);
-                    x = -1;
                     edges.emplace_back(a1, a2);
+//
+//                    Point a3((i + 1) * 10 - 3, (x + 1) * 10),
+//                            a4((i + 1) * 10 - 3, (j + 1) * 10);
+//                    edges.emplace_back(a3, a4);
+//
+//                    Point a5((i + 1) * 10, (x + 1) * 10),
+//                            a6((i + 1) * 10 - 3, (x + 1) * 10);
+//                    edges.emplace_back(a5, a6);
+//
+//                    Point a7((i + 1) * 10, (j + 1) * 10),
+//                            a8((i + 1) * 10 - 3, (j + 1) * 10);
+//                    edges.emplace_back(a7, a8);
+                    x = -1;
                 }
             }
         }
@@ -192,8 +220,20 @@ int32_t main() {
                 if (map[j][i] != map[j + 1][i]) {
                     Point a1((x + 1) * 10, (i + 1) * 10),
                             a2((j + 1) * 10, (i + 1) * 10);
-                    x = -1;
                     edges.emplace_back(a1, a2);
+//
+//                    Point a3((x + 1) * 10, (i + 1) * 10 - 3),
+//                            a4((j + 1) * 10, (i + 1) * 10 - 3);
+//                    edges.emplace_back(a3, a4);
+//
+//                    Point a5((x + 1) * 10, (i + 1) * 10),
+//                            a6((x + 1) * 10, (i + 1) * 10 - 3);
+//                    edges.emplace_back(a5, a6);
+//
+//                    Point a7((j + 1) * 10, (i + 1) * 10),
+//                            a8((j + 1) * 10, (i + 1) * 10 - 3);
+//                    edges.emplace_back(a7, a8);
+                    x = -1;
                 }
             }
         }
@@ -211,6 +251,17 @@ int32_t main() {
     Ray move;
     move.p = person.p, move.Vec = Point(-1, 0);
     output();
+
+
+    // для красоты
+
+    vector <ld> kof(W);
+    ld yg = -45 * PI / 180;
+    ld step = (ld(100) / W) * PI / 180, st2 =  (ld(90) / W) * PI / 180;
+    for (int i = 0; i < W; ++i) {
+        kof[i] = cos(yg);
+        yg += st2;
+    }
     while (true) {
         char c = getchar();
         if (c == 'w' || c == 'W') {
@@ -241,26 +292,66 @@ int32_t main() {
         Ray a = person;
         a.Vec.rotate(-50 * PI / 180);
         a.e = Line(a.p, a.p + a.Vec);
-        ld step = (ld(100) / W) * PI / 180;
-        for (int i = 0; i < W; ++i) {
+
+        ld dist = minDistance(edges, a);
+        dist = max(dist, ld(1));
+        int col_cl = min(round(6 * H / (dist * kof[0])), ld(H));
+        for (int j = 0; j < (H - col_cl + 1) / 2; ++j) {
+            screen[j][0] = 0;
+        }
+        for (int j = (H - col_cl + 1) / 2; j < (H - col_cl + 1) / 2 + col_cl; ++j) {
+            if (col_cl >= 10) {
+                screen[j][0] = 1;
+            } else if (col_cl >= 5) {
+                screen[j][0] = 3;
+            } else {
+                screen[j][0] = 4;
+            }
+        }
+        for (int j = (H - col_cl + 1) / 2 + col_cl; j < H; ++j) {
+            screen[j][0] = 2;
+        }
+
+        for (int i = 0; i < W - 1; i++) {
             // code
-            ld dist = minDistance(edges, a);
+            a.Vec.rotate(step);
+            a.e = Line(a.p, a.p + a.Vec);
+            ld dist = minDistance(edges, a), pr;
+            int g = indx;
             dist = max(dist, ld(1));
-            int col_cl = min(round(6.3 * H / dist), ld(H));
+            pr = dist;
+            int col_cl = min(round(6 * H / (dist * kof[i + 1])), ld(H));
             for (int j = 0; j < (H - col_cl + 1) / 2; ++j) {
-                screen[j][i] = 0;
+                screen[j][i + 1] = 0;
             }
             for (int j = (H - col_cl + 1) / 2; j < (H - col_cl + 1) / 2 + col_cl; ++j) {
-                if (col_cl >= 13) {
-                    screen[j][i] = 1;
-                } else if (col_cl >= 6) {
-                    screen[j][i] = 3;
+                if (col_cl >= 10) {
+                    screen[j][i + 1] = 1;
+                } else if (col_cl >= 5) {
+                    screen[j][i + 1] = 3;
                 } else {
-                    screen[j][i] = 4;
+                    screen[j][i + 1] = 4;
                 }
             }
             for (int j = (H - col_cl + 1) / 2 + col_cl; j < H; ++j) {
-                screen[j][i] = 2;
+                screen[j][i + 1] = 2;
+            }
+            a.Vec.rotate(-step);
+            a.e = Line(a.p, a.p + a.Vec);
+            dist = minDistance(edges, a);
+            dist = max(dist, ld(1));
+            if (g != indx || indx == -1) {
+                if (pr < dist) {
+                    col_cl = min(round(6 * H / (pr * kof[i + 1])), ld(H));
+                    for (int j = (H - col_cl + 1) / 2; j < (H - col_cl + 1) / 2 + col_cl; ++j) {
+                        screen[j][i + 1] = 69;
+                    }
+                } else {
+                    col_cl = min(round(6 * H / (dist * kof[i])), ld(H));
+                    for (int j = (H - col_cl + 1) / 2; j < (H - col_cl + 1) / 2 + col_cl; ++j) {
+                        screen[j][i] = 69;
+                    }
+                }
             }
             a.Vec.rotate(step);
             a.e = Line(a.p, a.p + a.Vec);
